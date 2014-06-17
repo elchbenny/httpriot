3c18d5116148de582eb4f49dde7dacaeb57eed7d
Benjamin Müller <benjamin@urbn.de>
2014-06-17 14:53:18 +0200

    added podspec

--- Source/HTTPRiot/HRRequestOperation.m
+++ Source/HTTPRiot/HRRequestOperation.m
@@ -194,6 +194,18 @@
     [self finish];
 }
 
+- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace {
+    return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge *)challenge {
+    
+    if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
+        [challenge.sender useCredential:[NSURLCredential credentialForTrust:challenge.protectionSpace.serverTrust] forAuthenticationChallenge:challenge];
+    
+    [challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark - Configuration
 
